10ad94f0f1dfa0dd650843d84ca68ccedac484e6
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-07-30 12:54:42 +0200

    created navbar with logo and some css style

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    include __DIR__ . "/Views/header.php";
+?>
+
+<header>
+    <div class="mt-nav">
+        <div class="container">
+            <nav class="d-flex justify-content-between align-items-center">
+                <img src="./img/logo.png" alt="logo" id="mt-logo">
+                <ul class="list-unstyled">
+                    <li class="mt-3"><a class="mt-delete" href="">Cancella Viaggio</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+
+<?php
+    include __DIR__ . "/Views/footer.php"
+?>
