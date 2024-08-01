5c284bd875f6b295d9be732a40c540c2bb6308cc
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-01 15:32:25 +0200

    added a div with id map that contains the map generated by tomtom

--- index.php
+++ index.php
@@ -110,7 +110,7 @@
                 <div class="card mb-4">
                     <div class="card-body">
                         <h5 class="card-title">Mappa</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div id="map" class="card-text"></div>
                     </div>
                 </div>
                 <!-- <div class="card mb-4">
@@ -126,6 +126,9 @@
     </div>
 </main>
 
+<script src="js/script.js" type="module"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.13.0/maps/maps-web.min.js"></script>
 <?php
     include __DIR__ . "/Views/footer.php"
 ?>
