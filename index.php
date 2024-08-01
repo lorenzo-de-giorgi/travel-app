973ac0ec9d5f40cdfd2c5b96b5859351ea10b83a
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-01 13:04:17 +0200

    created layout with card to divide all things

--- index.php
+++ index.php
@@ -16,7 +16,8 @@
 </header>
 
 <main>
-    <form action="./www/save_data.php" method="post">
+    <!-- ESEMPIO DI FORM DA SEGUIRE -->
+    <!-- <form action="./www/save_data.php" method="post">
         <label for="nome">Nome:</label>
         <input type="text" id="nome" name="nome" required><br><br>
 
@@ -27,7 +28,44 @@
         <textarea id="messaggio" name="messaggio" required></textarea><br><br>
 
         <input type="submit" value="Invia">
-    </form>
+    </form> -->
+    <div class="container">
+        <div class="row">
+            <!-- left card -->
+            <div class="col-md-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title">Crea Nuova Tappa</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title">Tappe Attive</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- right card -->
+            <div class="col-md-6">
+                <div class="card mb-1">
+                    <div class="card-body">
+                        <h5 class="card-title">Mappa</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <!-- <div class="card mb-4">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Card image 4">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title 4</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+    </div>
 </main>
 
 <?php
