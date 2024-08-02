61a677d9e361ba4bef5c6db4f88ab9d09819828a
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-02 10:48:49 +0200

    added users table to manage sessions

--- www/create_tables.php
+++ www/create_tables.php
@@ -1,14 +1,22 @@
 <?php
-require 'db.php';
+    require 'db.php';
 
     try {
         $sql = "
+        CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(255) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL
+        );
+
         CREATE TABLE IF NOT EXISTS travels (
             id INT AUTO_INCREMENT PRIMARY KEY,
+            user_id INT NOT NULL,
             title VARCHAR(255) NOT NULL,
             description TEXT,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
         );
 
         CREATE TABLE IF NOT EXISTS stages (
