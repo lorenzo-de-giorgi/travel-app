50d5bd4d36d64e4e237db86571960154b71829d7
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-06 12:41:54 +0200

    TEMPORARILY modified the database

--- www/create_tables.php
+++ www/create_tables.php
@@ -3,30 +3,15 @@
 
     try {
         $sql = "
-        CREATE TABLE IF NOT EXISTS users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            username VARCHAR(255) NOT NULL UNIQUE,
-            password VARCHAR(255) NOT NULL
-        );
 
-        CREATE TABLE IF NOT EXISTS travels (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            user_id INT NOT NULL,
-            title VARCHAR(255) NOT NULL,
-            description TEXT,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-        );
 
         CREATE TABLE IF NOT EXISTS stages (
             id INT AUTO_INCREMENT PRIMARY KEY,
-            travel_id INT NOT NULL,
             stage_name VARCHAR(255) NOT NULL,
+            stage_address VARCHAR(255) NOT NULL,
             stage_description TEXT,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (travel_id) REFERENCES travels(id) ON DELETE CASCADE
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );";
 
         $pdo->exec($sql);
@@ -34,3 +19,22 @@
     } catch (PDOException $e) {
         echo "Error creating tables: " . $e->getMessage();
     }
+
+    // travel_id INT NOT NULL,
+    // FOREIGN KEY (travel_id) REFERENCES travels(id) ON DELETE CASCADE
+
+    // CREATE TABLE IF NOT EXISTS users (
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     username VARCHAR(255) NOT NULL UNIQUE,
+    //     password VARCHAR(255) NOT NULL
+    // );
+
+    // CREATE TABLE IF NOT EXISTS travels (
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     user_id INT NOT NULL,
+    //     name VARCHAR(255) NOT NULL,
+    //     description TEXT,
+    //     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    //     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    //     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    // );
