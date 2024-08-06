40f2a66aea83bdf6ae8df28d8c998fd339e26904
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-06 18:23:16 +0200

    modified script.js to get dynamic stages to map

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { TOMTOM_API_KEY } from '../config.js';
+
 window.onload = function() {
     var map = tt.map({
         key: `${TOMTOM_API_KEY}`,
@@ -7,38 +8,26 @@ window.onload = function() {
         zoom: 10
     });
 
-    var markers = [
-        { lng: 12.4964, lat: 41.9028 }, // Roma
-        { lng: 12.3155, lat: 45.4408 }, // Venezia
-        { lng: 9.1900, lat: 45.4642 }   // Milano
-    ];
+    // Funzione per ottenere i marker dal server
+    function fetchStages() {
+        return fetch('api/stages.php') // Supponendo che l'API si trovi a questo endpoint
+            .then(response => response.json())
+            .then(data => {
+                return data.stages; // Adatta questa linea in base al formato della risposta JSON
+            });
+    }
 
-    markers.forEach(function(marker) {
-        new tt.Marker().setLngLat([marker.lng, marker.lat]).addTo(map);
-    });
-
-    var routeOptions = {
-        key: `${TOMTOM_API_KEY}`,
-        locations: '12.4964,41.9028:12.3155,45.4408:9.1900,45.4642'
-    };
-
-    tt.services.calculateRoute(routeOptions).go().then(function(routeData) {
-        var geojson = routeData.toGeoJson();
-        map.addLayer({
-            'id': 'route',
-            'type': 'line',
-            'source': {
-                'type': 'geojson',
-                'data': geojson
-            },
-            'layout': {
-                'line-cap': 'round',
-                'line-join': 'round'
-            },
-            'paint': {
-                'line-color': '#4a90e2',
-                'line-width': 6
-            }
+    // Funzione per aggiungere i marker alla mappa
+    function addStagesToMap(stages) {
+        stages.forEach(function(stage) {
+            new tt.Marker().setLngLat([stage.stage_longitude, stage.stage_latitude]).addTo(map);
         });
+    }
+
+    // Recupera i marker dal server e aggiungili alla mappa
+    fetchStages().then(stages => {
+        addStagesToMap(stages);
+    }).catch(error => {
+        console.error('Errore nel recupero dei marker:', error);
     });
 };
