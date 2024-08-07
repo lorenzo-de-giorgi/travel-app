76a1bb4ffe81b61bf9c6c501d5fbeb4b53ec4bf9
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-07 17:59:55 +0200

    imported tomtom api js and fixed js/script.js import

--- Views/header.php
+++ Views/header.php
@@ -10,9 +10,9 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link grity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <!-- tomtom map -->
-        <link rel="stylesheet" href="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.13.0/maps/maps.css">
-        <!-- javascript -->
-        <script src="js/script.js" type="module" defer></script>
+        <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.20.0/maps/maps-web.min.js"></script>
+        <link rel="stylesheet" href="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.20.0/maps/maps.css">
+        <script src="js/script.js" type="module" src="script.js"></script>
         <!-- css stylesheet -->
         <link rel="stylesheet" href="css/style.css">
         <title>Travel App</title>
