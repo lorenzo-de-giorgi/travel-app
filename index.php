4f7fd9db73add8ef49b20fbf8753ba72c3957263
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-08 12:58:58 +0200

    removed "Cancella Viaggio" button

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
             <nav class="d-flex justify-content-between align-items-center">
                 <img src="./img/logo.png" alt="logo" id="mt-logo">
                 <ul class="list-unstyled">
-                    <li class="mt-3"><a class="mt-delete" href="">Cancella Viaggio</a></li>
+                    <!-- <li class="mt-3"><a class="mt-delete" href="">Cancella Viaggio</a></li> -->
                 </ul>
             </nav>
         </div>
