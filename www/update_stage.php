2ef507d36d1deb5e6d48a34055ada4eac116cf90
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-13 13:46:25 +0200

    lightened code

--- www/update_stage.php
+++ www/update_stage.php
@@ -1,38 +1,28 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=travel-app_db';
-    $username = 'root';
-    $password = 'root';
+    require 'db.php';
+    // Verifica che il parametro ID sia presente
+    if (isset($_POST['id'])) {
+        $stageId = $_POST['id'];
     
-    try {
-        $pdo = new PDO($dsn, $username, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // Recupera lo stato attuale di `stage_completed`
+        $sql = 'SELECT stage_completed FROM stages WHERE id = ?';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$stageId]);
+        $stage = $stmt->fetch(PDO::FETCH_ASSOC);
     
-        // Verifica che il parametro ID sia presente
-        if (isset($_POST['id'])) {
-            $stageId = $_POST['id'];
+        if ($stage) {
+            $newStatus = $stage['stage_completed'] == 0 ? 1 : 0;
     
-            // Recupera lo stato attuale di `stage_completed`
-            $sql = 'SELECT stage_completed FROM stages WHERE id = ?';
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute([$stageId]);
-            $stage = $stmt->fetch(PDO::FETCH_ASSOC);
+            // Aggiorna lo stato di `stage_completed`
+            $updateSql = 'UPDATE stages SET stage_completed = ? WHERE id = ?';
+            $updateStmt = $pdo->prepare($updateSql);
+            $updateStmt->execute([$newStatus, $stageId]);
     
-            if ($stage) {
-                $newStatus = $stage['stage_completed'] == 0 ? 1 : 0;
-    
-                // Aggiorna lo stato di `stage_completed`
-                $updateSql = 'UPDATE stages SET stage_completed = ? WHERE id = ?';
-                $updateStmt = $pdo->prepare($updateSql);
-                $updateStmt->execute([$newStatus, $stageId]);
-    
-                header('Location: ' . $_SERVER['HTTP_REFERER']);
-                exit();
-            } else {
-                echo 'Tappa non trovata.';
-            }
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+            exit();
         } else {
-            echo 'ID non specificato.';
+            echo 'Tappa non trovata.';
         }
-    } catch (PDOException $e) {
-        echo '<p class="text-danger">Errore: ' . $e->getMessage() . '</p>';
+    } else {
+        echo 'ID non specificato.';
     }
