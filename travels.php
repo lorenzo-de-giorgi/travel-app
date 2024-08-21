7050f2c410564a816c24e684b5ca7eb36d059a89
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-21 17:11:50 +0200

    modified travels.php page with card organizations

--- travels.php
+++ travels.php
@@ -40,48 +40,70 @@
 
 <!DOCTYPE html>
 <html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>I Miei Viaggi</title>
-</head>
-<body>
-    <header>
-        <div class="mt-nav">
-            <div class="container">
-                <nav class="d-flex justify-content-between align-items-center">
-                    <img src="./img/logo.png" alt="logo" id="mt-logo">
-                    <ul class="list-unstyled">
-                        <!-- <li class="mt-3"><a class="mt-delete" href="">Cancella Viaggio</a></li> -->
-                    </ul>
-                </nav>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/style.css">
+        <title>I Miei Viaggi</title>
+    </head>
+    <body>
+        <header>
+            <div class="mt-nav">
+                <div class="container">
+                    <nav class="d-flex justify-content-between align-items-center">
+                        <img src="./img/logo.png" alt="logo" id="mt-logo">
+                        <ul class="list-unstyled">
+                            <!-- <li class="mt-3"><a class="mt-delete" href="">Cancella Viaggio</a></li> -->
+                        </ul>
+                    </nav>
+                </div>
             </div>
-        </div>
-    </header>
-    <h2>I Miei Viaggi</h2>
-    <ul>
-        <?php if (!empty($trips)): ?>
-            <?php foreach ($trips as $trip): ?>
-                <li>
-                    <a href="travel.php?id=<?php echo $trip['id']; ?>"><?php echo htmlspecialchars($trip['name']); ?></a>
-                </li>
-            <?php endforeach; ?>
-        <?php else: ?>
-            <li>Non hai creato nessun viaggio.</li>
-        <?php endif; ?>
-    </ul>
-    <a href="">Crea Un viaggio</a>
-    <form action="./www/create_travel.php" method="POST">
-        <div class="mb-3">
-            <label for="name" class="form-label">Nome Viaggio</label>
-            <input placeholder="Inserisci il nome del viaggio da creare" type="text" class="form-control" id="name" name="name" required>
-        </div>
-        <div class="mb-3">
-            <label for="description" class="form-label">Descrizione Viaggio</label>
-            <input placeholder="Inserisci la descrizione del viaggio da creare" type="text" class="form-control" id="description" name="description" required>
-        </div>
-        <button type="submit" class="btn btn-primary mt-3">Submit</button>
-    </form>
-</body>
+        </header>
+
+        <main>
+            <div class="container">
+                <h2 class="text-center">I Miei Viaggi</h2>
+                <div class="row mt-5">
+                    <div class="col-6">   
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <h5 class="card-title">Crea Nuovo Viaggio</h5>
+                                <p class="card-text">
+                                    <?php if (!empty($trips)): ?>
+                                        <?php foreach ($trips as $trip): ?>
+                                            <li>
+                                                <a href="travel.php?id=<?php echo $trip['id']; ?>"><?php echo htmlspecialchars($trip['name']); ?></a>
+                                            </li>
+                                        <?php endforeach; ?>
+                                    <?php else: ?>
+                                        <hai>Non hai ancora nessun viaggio creato!</h6>
+                                    <?php endif; ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <h5 class="card-title">Crea Nuovo Viaggio</h5>
+                                <p class="card-text">
+                                    <form action="./www/create_travel.php" method="POST">
+                                        <div class="mb-3">
+                                            <label for="name" class="form-label">Nome Viaggio</label>
+                                            <input placeholder="Inserisci il nome del viaggio da creare" type="text" class="form-control" id="name" name="name" required>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="description" class="form-label">Descrizione Viaggio</label>
+                                            <input placeholder="Inserisci la descrizione del viaggio da creare" type="text" class="form-control" id="description" name="description" required>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary mt-3">Crea</button>
+                                    </form>
+                                </p>
+                            </div>
+                        </div>       
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
 </html>
