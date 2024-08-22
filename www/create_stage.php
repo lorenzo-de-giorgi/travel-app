46366965a91995806ff6f760aaaf7756523d4cab
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-22 12:21:32 +0200

    change redirect link so that it takes you to the trip detail plan

--- www/create_stage.php
+++ www/create_stage.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Salvataggio Dati</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <?php
+        $redirect_url = isset($_POST['travel_id']) ? '../travel.php?id=' . htmlspecialchars($_POST['travel_id']) : '../travel.php';
+    ?>
     <!-- tage per redirect dopo 5 secondi -->
-    <meta http-equiv="refresh" content="5;url=../index.php">
+    <meta http-equiv="refresh" content="5;url=<?php echo $redirect_url; ?>">
 </head>
 <body>
     <div class="container mt-5">
