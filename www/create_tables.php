1a763598c380033d07c055d458e612013d5163e3
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-23 11:38:31 +0200

    added name and email field in users table

--- www/create_tables.php
+++ www/create_tables.php
@@ -15,6 +15,8 @@
         
         CREATE TABLE IF NOT EXISTS users (
             id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(50) NULLABLE,
+            email VARCHAR(255) NULLABLE,
             username VARCHAR(255) NOT NULL UNIQUE,
             password VARCHAR(255) NOT NULL
         );
