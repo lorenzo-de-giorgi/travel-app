5b5cde9754fbba0f3a8e6e56c3597b39c54ff0a9
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-27 14:56:42 +0200

    modified header to go to homepage.php if the user is not registered

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     // controllo che l'utente sia loggato
     if(!isset($_SESSION['user_id'])){
         // se l'utente non è loggato rimanda al login
-        header("Location: www/auth/login.php");
+        header("Location: homepage.php");
         exit();
     } else {
         // se l'utente è loggato rimanda alla pagina dei viaggi
