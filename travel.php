da9d90d700d4212473c40ea4d76f8e9816f7d211
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-01 17:59:54 +0200

    added button and style to go back to travels.php page in the nav

--- travel.php
+++ travel.php
@@ -40,13 +40,30 @@
     $conn->close();
 ?>
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link grity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- tomtom map -->
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.20.0/maps/maps-web.min.js"></script>
+    <link rel="stylesheet" href="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.20.0/maps/maps.css">
+    <script src="js/script.js" type="module" src="script.js"></script>
+    <!-- css stylesheet -->
+    <link rel="stylesheet" href="./css/travel-page.css">
+    <title>Travel App</title>
+</head>
+
 <header>
     <div class="mt-nav">
         <div class="container">
             <nav class="d-flex justify-content-between align-items-center">
                 <img src="./img/logo.png" alt="logo" id="mt-logo">
                 <ul class="list-unstyled">
-                    <!-- <li class="mt-3"><a class="mt-delete" href="">Cancella Viaggio</a></li> -->
+                    <li class="mt-3"><a class="mt-btn" href="./travels.php"><i class="fa-solid fa-arrow-left"></i> Indietro</a></li>
                 </ul>
             </nav>
         </div>
